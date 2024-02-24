5afa02c7966a6aa44b204f585ebbbe02c4f3be5a
gamalielara <aragamalielboanerges@gmail.com>
2024-02-24 14:13:01 +0700

    delete unused comps

---
+++ src/screens/WeeklyScreen/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+import { BaseText, BoldText } from '../../components/global/text';
+
+export const Container = styled.View`
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+`;
+
+export const WeeklyTitleText = styled(BoldText)`
+  font-size: 30px;
+  margin-bottom: 10px;
+`;
