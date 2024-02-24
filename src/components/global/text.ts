bf192ad2f7080d31d5a38e4b5f930403eb62d955
gamalielara <aragamalielboanerges@gmail.com>
2024-02-24 14:05:33 +0700

    fix font

--- src/components/global/text.ts
+++ src/components/global/text.ts
@@ -1,14 +1,14 @@
-import { styled } from "styled-components/native";
+import { styled } from 'styled-components/native';
 
 export const BaseText = styled.Text`
-  font-family: "Inter";
-  color: ${(props) => props.theme.text};
+  font-family: 'Inter-Regular';
+  color: ${props => props.theme.text};
 `;
 
 export const BoldText = styled(BaseText)`
-  font-family: "Inter-Bold";
+  font-family: 'Inter-Bold';
 `;
 
 export const BaseTextInput = styled.TextInput`
-  font-family: Inter;
+  font-family: 'Inter-Regular';
 `;
