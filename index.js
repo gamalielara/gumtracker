e987e237526510db19c1cb2a965857b598de33ee
gamalielara <aragamalielboanerges@gmail.com>
2024-03-31 19:55:26 +0700

    init msw

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 
+if (__DEV__) {
+  require('./msw.polyfills');
+  const { server } = require('./src/mock/nativeServer');
+  server.listen();
+}
+
 AppRegistry.registerComponent(appName, () => App);
